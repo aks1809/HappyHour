962c209640c496289a2db8be8b8c03a84f37cfa7
ShreyanshAcharya <shreyanshacharya09@gmai.com>
2022-02-16 11:30:30 +0530

    initial commit

---
+++ login-register-MERN-LocalStorage-master/login-register-MERN-LocalStorage-master/login-and-register-backend/index.js
@@ -0,0 +1,69 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cors())
+const DB = 'mongodb+srv://Shreyansh:Shreyansh@cluster0.5gauy.mongodb.net/myLoginRegisterDB?retryWrites=true&w=majority';
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+    console.log("DB connected");
+}).catch((err)=>console.log('no connection'));
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    password: String
+})
+
+const User = new mongoose.model("User", userSchema)
+
+//Routes
+app.post("/login", (req, res)=> {
+    const { email, password} = req.body
+    User.findOne({ email: email}, (err, user) => {
+        if(user){
+            if(password === user.password ) {
+                res.send({message: "Login Successfull", user: user})
+            } else {
+                res.send({ message: "Password didn't match"})
+            }
+        } else {
+            res.send({message: "User not registered"})
+        }
+    })
+}) 
+
+app.post("/register", (req, res)=> {
+    const { name, email, password} = req.body
+    User.findOne({email: email}, (err, user) => {
+        if(user){
+            res.send({message: "User already registerd"})
+        } else {
+            const user = new User({
+                name,
+                email,
+                password
+            })
+            user.save(err => {
+                if(err) {
+                    res.send(err)
+                } else {
+                    res.send( { message: "Successfully Registered, Please login now." })
+                }
+            })
+        }
+    })
+    
+}) 
+
+app.listen(9002,() => {
+    console.log("BE started at port 9002")
+})
